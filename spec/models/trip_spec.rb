1d942131f96600c1ec4f7d1cc03506a5c1035f1a
Artem Gusev <a.gus@engineeriam.net>
2015-07-24 10:56:31 +0300

    trips spec

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create!(email: 'test@mail.com', password: '123')
+  } 
+
+  let(:valid_attributes) {
+    {
+      destination: 'Brasilia',
+      start_date: Time.now.to_date,
+      end_date: Time.now.to_date + 7.days,
+      user_id: user.id
+    }
+  }
+
+  it "requires destination field presence" do
+    trip = Trip.create valid_attributes.merge(destination: nil)
+    expect(trip.errors.size).to eq(1)
+  end
+
+  it "requires start_date field presence" do
+    trip = Trip.create valid_attributes.merge(start_date: nil)
+    expect(trip.errors.size).to eq(1)
+  end
+
+  it "requires end_date presence" do
+    trip = Trip.create valid_attributes.merge(end_date: nil)
+    expect(trip.errors.size).to eq(1)
+  end
+
+  it "requires user_id field presence" do
+    trip = Trip.create valid_attributes.merge(user_id: nil)
+    expect(trip.errors.size).to eq(1)
+  end
+
+  it "requires end_date to be greater than the start_date" do
+    trip = Trip.create valid_attributes.merge(end_date: Time.now.to_date - 1.day)
+    expect(trip.errors.size).to eq(1)
+  end
+
 end
