f8979921f9c774fc9ff742713ddbbca0272672a5
Artem Gusev <a.gus@engineeriam.net>
2015-07-24 11:37:49 +0300

    users spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {
+      email: 'test@email.com',
+      rank: 'admin',
+      password: '123'
+    }
+  }
+
+  it "creates user with valid attributes" do
+    user = User.create! valid_attributes
+    expect(user.id).to_not eq(nil)
+  end
+
+  it "requires email field presence" do
+    user = User.create valid_attributes.merge(email: nil)
+    expect(user.errors.size).to eq(2)
+  end
+
+  it "check if email address valied" do
+    user = User.create valid_attributes.merge(email: 'not_email')
+    expect(user.errors.size).to eq(1)
+  end
+
+  it "requires rank correctness" do
+    user = User.create valid_attributes.merge(rank: 'random')
+    expect(user.errors.size).to eq(1)
+  end
+
+  it "destroys dependant trips" do
+    user = User.create! valid_attributes
+    trip = Trip.create!({
+      destination: 'Honk Kong',
+      start_date: Time.now.to_date,
+      end_date: Time.now.to_date + 7.days,
+      user_id: user.id
+    })
+
+    user.destroy
+    expect{ trip.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
 end
